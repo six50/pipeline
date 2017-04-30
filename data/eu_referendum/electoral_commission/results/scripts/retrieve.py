fd49980563eb1f4c53a48c8cde2f7d8cdb23fde8
John Sandall <john.sandall@gmail.com>
2017-04-30 18:06:53 +0100

    Add EU referendum results

---
+++ data/eu_referendum/electoral_commission/results/scripts/retrieve.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import requests
+import zipfile
+
+# Config
+url = 'http://www.electoralcommission.org.uk/__data/assets/file/0014/212135/'
+filename = 'EU-referendum-result-data.csv'
+target = Path('../raw/') / filename
+
+# Download URL into local directory
+print('Downloading into {}'.format(target.resolve()))
+with open(target, 'wb') as f:
+    response = requests.get(url + filename)
+    f.write(response.content)
