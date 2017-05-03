2fbe1692cadbff909dcecf245b3c0cc8c0188916
John Sandall <john.sandall@gmail.com>
2017-05-03 08:08:48 +0100

    Change polling data source

--- data/polls/generate_json.py
+++ data/polls/generate_json.py
@@ -1,6 +1,7 @@
 import feather
 import pandas as pd
 from pathlib import Path
+import statsmodels.api as sm
 from subprocess import DEVNULL, STDOUT, check_call
 
 
@@ -9,29 +10,25 @@ if __name__ == '__main__':
     DATA_DIR = Path('.') / 'data'
 
     print('Downloading from Google Sheet')
-    url = "https://docs.google.com/spreadsheets/d/1CHMArwUdVza-ayOT1aG2tRJJfa1OWvwfMGiCu20X7Ys/export?gid=292271149&format=csv"
+    url = "https://docs.google.com/spreadsheets/d/1CHMArwUdVza-ayOT1aG2tRJJfa1OWvwfMGiCu20X7Ys/export?gid=1495247060&format=csv"
     polls = pd.read_csv(url)
 
     # PROCESS
-    # Separate out pollster & publisher
-    polls['Publisher'] = polls.Pollster.apply(lambda x: x.split('/')[-1])
-    polls['Pollster'] = polls.Pollster.apply(lambda x: x.split('/')[0])
-
-    # Split out dates
-    polls['Sampled From'] = polls.Dates.apply(lambda x: x.split(' - ')[0])
-    polls['Sampled To'] = polls.Dates.apply(lambda x: x.split(' - ')[-1])
-
-    # Remove commas from sample size
-    polls['Sample size'] = polls['Sample size'].apply(lambda x: float(x.replace(",", "")))
-
     # Format percentages into decimal
-    for col in ['CON', 'LAB', 'LIB', 'UKIP', 'Green', 'Other', 'Total']:
+    for col in ['CON', 'LAB', 'LD', 'UKIP', 'GRN', 'Other']:
         polls[col] = polls[col].apply(lambda x: x / 100)
+
+    # Process dates
+    polls['To'] = pd.to_datetime(polls['To'])
 
     # Rename & rearrange columns
     polls.columns = [x.lower().replace(" ", "_") for x in polls.columns]
-    polls = polls[['pollster', 'publisher', 'sampled_from', 'sampled_to', 'sample_size',
-                   'con', 'lab', 'lib', 'ukip', 'green', 'other', 'total', 'source']]
+    polls.columns = ['date' if x == 'to' else x for x in polls.columns]
+    polls = polls[['company', 'client', 'date', 'con', 'lab', 'ld', 'ukip', 'grn']]
+
+    # Add LOWESS smoothing
+    #for col in ['con', 'lab', 'ld']:
+    #    polls[col + '_smooth'] = sm.nonparametric.lowess(polls[col], polls['date'], frac=0.1)[:, 1]
 
     # EXPORT
     polls.to_json(str(DATA_DIR / 'polls.json'), orient='records')
