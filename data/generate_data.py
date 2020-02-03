526184494c5b0eb58a6556bfd40d3ec259c5b891
John Sandall <john@coefficient.ai>
2020-02-03 20:14:48 +0000

    Comment out broken pipelines for now (soon to be replaced with maven anyway as this repo slowly becomes deprecated) + some black

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,24 +1,27 @@
 from pathlib import Path
 
 from eu_referendum.electoral_commission.results.scripts import retrieve as eu_retrieve
-from general_election.electoral_commission.results.scripts import (retrieve_2010, retrieve_2015,
-                                                                   process_2010, process_2015)
+from general_election.electoral_commission.results.scripts import (
+    process_2010,
+    process_2015,
+    retrieve_2010,
+    retrieve_2015,
+)
 from model.scripts import process as model_process
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     #  Retrieve EU Referendum data
-    eu_path = Path('.') / 'data' / 'eu_referendum' / 'electoral_commission' / 'results'
+    eu_path = Path(".") / "data" / "eu_referendum" / "electoral_commission" / "results"
     eu_retrieve.main(eu_path)
 
     # Retrieve & clean general election data
-    ge_path = Path('.') / 'data' / 'general_election' / 'electoral_commission' / 'results'
+    ge_path = Path(".") / "data" / "general_election" / "electoral_commission" / "results"
     retrieve_2010.main(ge_path)
-    retrieve_2015.main(ge_path)
-    process_2010.main(ge_path)
-    process_2015.main(ge_path)
+    # retrieve_2015.main(ge_path)  # TODO: This is broken.
+    # process_2010.main(ge_path)  # TODO: This is broken.
+    # process_2015.main(ge_path)  # TODO: This is broken.
 
     # Process data ready for modelling
-    model_path = Path('.') / 'data'
-    model_process.main(model_path)
+    model_path = Path(".") / "data"
+    # model_process.main(model_path)  # TODO: This is broken.
